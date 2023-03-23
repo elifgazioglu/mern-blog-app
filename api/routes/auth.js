ac0d5096a632f384131fd84dce076d3404096759
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-03-23 12:47:18 +0300

    hashing password

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 //Register
 
 router.post("/register", async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPass,
     });
 
     const user = await newUser.save();
@@ -19,6 +22,5 @@ router.post("/register", async (req, res) => {
 });
 
 //Login
-
 
 module.exports = router;
