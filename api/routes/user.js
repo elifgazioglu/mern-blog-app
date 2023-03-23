e38ef17b8952761d9ceb5241b68fad8935b9076e
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-03-24 00:38:03 +0300

    user get process

--- api/routes/user.js
+++ api/routes/user.js
@@ -47,4 +47,15 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+//Get User
+router.get("/:id", async(req,res) => {
+    try{
+        const user = await User.findById(req.params.id)
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
 module.exports = router;
