285831cafd7400eb7bda0d0aa91cfb9d4b17275d
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-03-23 13:07:43 +0300

    create login route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -22,5 +22,18 @@ router.post("/register", async (req, res) => {
 });
 
 //Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong credentials");
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Wrong credentials");
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
