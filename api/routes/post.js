6d7dcb0668ae5608c0e6779804a9ea7d61dca683
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-03-24 23:07:59 +0300

    post delete process

--- api/routes/post.js
+++ api/routes/post.js
@@ -39,6 +39,24 @@ router.put("/:id", async (req, res) => {
 });
 
 //Delete Post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json("Post has been deleted...");
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can delete only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //Get Post
 router.get("/:id", async (req, res) => {
