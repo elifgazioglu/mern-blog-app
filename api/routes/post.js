db7052cdb4c2a387569f7da53ed88e0f56c7a7c2
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-03-24 22:38:21 +0300

    post update process

--- api/routes/post.js
+++ api/routes/post.js
@@ -14,22 +14,27 @@ router.post("/", async (req, res) => {
 });
 
 //Update Post
-router.delete("/:id", async (req, res) => {
-  if (req.body.userId == req.params.id) {
-    try {
-      const user = await User.findById(req.params.id);
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
       try {
-        await Post.deleteMany({ username: user.username });
-        await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User has been deleted...");
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
       } catch (err) {
         res.status(500).json(err);
       }
-    } catch (err) {
-      res.status(404).json("User not found!");
+    } else {
+      res.status(401).json("You can update only your post");
     }
-  } else {
-    res.status(401).json("You can delete only your account");
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
