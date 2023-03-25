cfc1f6e21e1742b224f43069b6fabab1f14621b3
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-03-25 23:11:48 +0300

    post get post

--- api/routes/post.js
+++ api/routes/post.js
@@ -61,9 +61,32 @@ router.delete("/:id", async (req, res) => {
 //Get Post
 router.get("/:id", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get All Posts
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username: username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      //if there is no condition get all posts
+      posts = await Post.find();
+    }
+    res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err);
   }
