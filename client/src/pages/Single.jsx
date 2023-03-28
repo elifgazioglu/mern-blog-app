ded38c994b99b80fb2c4278695351a5793cc453f
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-03-28 23:54:24 +0300

    add icons posts and Menu component

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { AiOutlineEdit } from "react-icons/ai";
 import { AiOutlineDelete } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { Menu } from "../components/Menu";
 
 const Single = () => {
   return (
@@ -13,7 +14,7 @@ const Single = () => {
         />
         <div className="user">
           <img
-            src="https://img.mensxp.com/media/content/2020/Aug/AMP_imghappy-girl-taking-a-selfie_5f4a2e4438bf3.jpeg"
+            src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
             alt=""
           />
           <div className="info">
@@ -21,16 +22,48 @@ const Single = () => {
             <p>Posted 2 days ago</p>
           </div>
           <div className="edit">
-            <Link>
-              <AiOutlineEdit></AiOutlineEdit>
+            <Link className="link" to={"/write?edit=2"}>
+              <AiOutlineEdit className="icon"></AiOutlineEdit>
             </Link>
-            <Link>
-              <AiOutlineDelete></AiOutlineDelete>
+            <Link className="link">
+              <AiOutlineDelete className="icon"></AiOutlineDelete>
             </Link>
           </div>
         </div>
+        <h1>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus,
+          rerum?
+        </h1>
+        <p1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+          qui sint vitae sequi consequuntur alias est, quo provident distinctio
+          maiores rem laboriosam nemo hic aut dolore eius illum commodi.Impedit
+          nesciunt ea nam debitis ratione non quibusdam odio, optio molestiae
+          omnis harum suscipit soluta ducimus dolorum inventore autem obcaecati
+          officiis repudiandae esse nihil, iste tenetur quia ex explicabo.
+          <br></br>
+          <br></br>
+          Natus quod harum tenetur veritatis fugit hic animi laudantium rem
+          aliquid eius inventore velit quibusdam ratione laborum molestias nisi
+          culpa explicabo earum, tempore officia nostrum et perspiciatis! Omnis
+          hic sequi, at dolorem necessitatibus laboriosam quas distinctio
+          veritatis ullam accusantium atque fuga debitis beatae aut culpa ex
+          quisquam similique eveniet aliquid ad fugit deleniti? Ratione porro
+          facilis fugit sit iste, in perspiciatis modi aut, qui ipsa commodi
+          tenetur ad quasi aspernatur odit.
+          <br></br>
+          <br></br>Mollitia, saepe in dicta harum tempore quaerat ullam quas
+          molestias ipsam dignissimos sequi blanditiis voluptas commodi,
+          laudantium possimus sit doloremque id distinctio quam sed nobis enim
+          eum temporibus maiores? Optio voluptatibus officia dolorem quod
+          necessitatibus. Atque libero deleniti autem quo voluptatibus. Incidunt
+          earum tempore inventore at minima ratione fuga non officiis doloribus
+          mollitia vitae natus quaerat magni aspernatur ipsum veniam sint,
+          nesciunt qui. Inventore ex, voluptatibus eos accusantium doloribus
+          reprehenderit id.
+        </p1>
       </div>
-      <div className="menu">m</div>
+      <Menu></Menu>
     </div>
   );
 };
