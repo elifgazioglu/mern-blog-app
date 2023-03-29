1486f5d5caee30e5b6a055935586c5d574ba8a5e
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-03-30 00:30:21 +0300

    register process with axios

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/auth/register", formData);
+      console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="auth">
       <h1>Register</h1>
-      <form>
-        <input required type="text" placeholder="username" />
-        <input required type="email" placeholder="email" />
-        <input required type="password" placeholder="password" />
-        <button>Register</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          placeholder="username"
+        />
+        <input
+          required
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="email"
+        />
+        <input
+          required
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="password"
+        />
+        <button type="submit">Register</button>
         <p>This is an error!</p>
         <span>
           Do you have any account? <Link to="/login">Login</Link>
