85a0a81b901ef8094f6d1feed7dbdc64128471ca
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-03-30 04:46:53 +0300

    email control for backend

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -36,4 +36,14 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.get('/email/:email', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.params.email });
+    res.json({ user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
