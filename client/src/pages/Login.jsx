9ebce4344273845c0113c4102709d01cff1c5a42
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-05-04 23:18:36 +0300

    add Login component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,15 +1,62 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import { useState, React } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        "http://localhost:5005/api/auth/login",
+        formData
+      );
+      setFormData({ email: "", password: "" });
+      console.log(res.data);
+      navigate("/home");
+    } catch (err) {
+      console.log(err);
+      setErrorMessage("Invalid email or password");
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 5000);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="auth">
       <h1>Login</h1>
-      <form>
-        <input required type="text" placeholder="username" />
-        <input required type="password" placeholder="password" />
-        <button>Login</button>
-        <p>This is an error!</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="email"
+        />
+        <input
+          required
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="password"
+        />
+        <button type="submit">Login</button>
+        {errorMessage && <p>{errorMessage}</p>}
         <span>
           Dont't you have an account? <Link to="/register">Register</Link>
         </span>
