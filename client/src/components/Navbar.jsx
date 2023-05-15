9431ab42f6acf690afe75846086c49c9b12d5471
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-05-15 14:44:48 +0300

    logout process using context api

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,42 +1,65 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import Logo from '../images/Logo.JPG'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../images/Logo.JPG";
+import { Context } from "../context/Context";
 
 const Navbar = () => {
+  const { user, dispatch } = useContext(Context);
+
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="container">
         <div className="logo">
           <img src={Logo} alt="" />
         </div>
         <div className="links">
-          <Link className='link' to="/?cat=art">
+          <Link className="link" to="/?cat=art">
             <h6>ART</h6>
           </Link>
-          <Link className='link' to="/?cat=science">
+          <Link className="link" to="/?cat=science">
             <h6>SCIENCE</h6>
           </Link>
-          <Link className='link' to="/?cat=technology">
+          <Link className="link" to="/?cat=technology">
             <h6>TECHNOLOGY</h6>
           </Link>
-          <Link className='link' to="/?cat=cinema">
+          <Link className="link" to="/?cat=cinema">
             <h6>CINEMA</h6>
           </Link>
-          <Link className='link' to="/?cat=design">
+          <Link className="link" to="/?cat=design">
             <h6>DESIGN</h6>
           </Link>
-          <Link className='link' to="/?cat=food">
+          <Link className="link" to="/?cat=food">
             <h6>FOOD</h6>
           </Link>
-          <span>Elif</span>
-          <span>Logout</span>
-          <span className='write'>
-            <Link className='link' to="/write">Write</Link>
-          </span>
+          {user ? ( // Kullanıcı giriş yapmışsa
+            <>
+              <span>{user.username}</span>{" "}
+              {/* Kullanıcının adını burada görüntüle */}
+              <span onClick={handleLogout}>Logout</span>
+              <span className="write">
+                <Link className="link" to="/write">
+                  Write
+                </Link>
+              </span>
+            </>
+          ) : (
+            <>
+              <Link className="link" to="/login">
+                Login
+              </Link>
+              <Link className="link" to="/register">
+                Register
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/context/Reducer.js
+++ client/src/context/Reducer.js
@@ -18,6 +18,12 @@ const Reducer = (state, action) => {
         isFetching: false,
         error: true,
       };
+      case "LOGOUT":
+      return {
+        user: null,
+        isFetching: false,
+        error: false,
+      };
     default:
       return state;
   }
