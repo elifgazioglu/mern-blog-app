fd1ca457bd94f5d8044bcbf4baab72d8277965e9
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-05-20 17:55:57 +0300

    added link elemnt to logo img

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     <div className="navbar">
       <div className="container">
         <div className="logo">
-          <img src={Logo} alt="" />
+          <Link to="/"><img src={Logo} alt="" /></Link>
         </div>
         <div className="links">
           <Link className="link" to="/?cat=art">
