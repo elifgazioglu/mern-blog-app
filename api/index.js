18ce44e1903b9b0d803ea0ca68e1b14bcaa9e6bb
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-06-24 21:52:00 +0300

    add file

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 const postRoute = require("./routes/post");
 const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 app.use(express.json());
 app.use(cors());
+app.use("/images", express.static(path.join(__dirname, "/images")))
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -21,10 +24,24 @@ mongoose
     console.error(err);
   });
 
-  app.use("/api/auth", authRoute)
-  app.use("/api/user", userRoute)
-  app.use("/api/post", postRoute)
-  app.use("/api/categories", categoryRoute)
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("file has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/post", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5005", () => {
   console.log("Backend is running");
